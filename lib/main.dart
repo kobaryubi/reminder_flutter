deef33bb5a144315d41d1f760e779c9fe86ee6f6
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-05-08 21:41:15 +0900

    feat: add list tile

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World!"),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.alarm),
+              title: Text("10:00"),
+            ),
+          ],
         ),
       ),
     );
