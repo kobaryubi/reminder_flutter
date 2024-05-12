058f8ea8d548ec40a249ed071ca5d93bae469ce9
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-05-12 21:28:45 +0900

    feat: view model

--- lib/view_model/reminder_view_model.dart
+++ lib/view_model/reminder_view_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-class ReminderModel extends ChangeNotifier {
+class ReminderViewModel extends ChangeNotifier {
   final List<String> reminders = [
     "10:00 AM - Meeting with the team",
     "12:00 PM - Lunch with John",
