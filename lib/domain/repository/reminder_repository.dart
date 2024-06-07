170bec762f765da5ca35259f17e7bedba70c4a45
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-06-08 08:59:48 +0900

    feat: add reminder

--- lib/domain/repository/reminder_repository.dart
+++ lib/domain/repository/reminder_repository.dart
@@ -2,4 +2,5 @@ import 'package:reminder_flutter/domain/entity/reminder_entity.dart';
 
 abstract class ReminderRepository {
   Future<List<ReminderEntity>> getReminders();
+  Future<void> addReminder(ReminderEntity reminderEntity);
 }
