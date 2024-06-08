d234799f0ef114a6c33260ece1e1f3c383af9e4b
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-06-08 11:32:36 +0900

    feat: add

--- lib/domain/entity/reminder_entity.dart
+++ lib/domain/entity/reminder_entity.dart
@@ -1,5 +1,5 @@
 class ReminderEntity {
-  final int id;
+  final String id;
   final String title;
 
   const ReminderEntity({
--- lib/presentation/screen/reminder_add_screen.dart
+++ lib/presentation/screen/reminder_add_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:reminder_flutter/application/use_case/reminder/add_reminder_use_case.dart';
+import 'package:reminder_flutter/domain/entity/reminder_entity.dart';
 
 class ReminderAddScreen extends StatefulWidget {
   const ReminderAddScreen({super.key});
@@ -10,6 +13,13 @@ class ReminderAddScreen extends StatefulWidget {
 
 class _ReminderAddScreenState extends State<ReminderAddScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,11 +51,23 @@ class _ReminderAddScreenState extends State<ReminderAddScreen> {
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
                 child: ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (!_formKey.currentState!.validate()) {
                       return;
                     }
 
+                    final addReminderUseCase =
+                        context.read<AddReminderUseCase>();
+                    final reminderEntity = ReminderEntity(
+                      id: '',
+                      title: _controller.text,
+                    );
+                    await addReminderUseCase(reminderEntity);
+                    _controller.clear();
+
+                    if (!context.mounted) {
+                      return;
+                    }
                     context.pop();
                   },
                   child: const Text('Submit'),
