8095b7348ef3fa844fb18c753a861699369d5df1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-06-09 10:03:58 +0900

    feat: remind at

--- lib/domain/entity/reminder_entity.dart
+++ lib/domain/entity/reminder_entity.dart
@@ -1,9 +1,11 @@
 class ReminderEntity {
   final String id;
   final String title;
+  final DateTime remindAt;
 
   const ReminderEntity({
     required this.id,
     required this.title,
+    required this.remindAt,
   });
 }
