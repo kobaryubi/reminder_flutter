618a2869a17065a91fba961c84e7b011960f59ec
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-06-11 22:05:02 +0900

    feat: edit

--- lib/presentation/screen/reminder_edit_screen.dart
+++ lib/presentation/screen/reminder_edit_screen.dart
@@ -1,11 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:reminder_flutter/application/use_case/reminder/add_reminder_use_case.dart';
+import 'package:reminder_flutter/application/use_case/reminder/get_reminder_use_case.dart';
 import 'package:reminder_flutter/domain/entity/reminder_entity.dart';
-import 'package:reminder_flutter/presentation/state/reminder_list_state.dart';
 import 'package:reminder_flutter/presentation/state/user_state.dart';
+import 'package:reminder_flutter/presentation/widget/reminder_edit_form_widget.dart';
 
 class ReminderEditScreen extends StatefulWidget {
   final String? id;
@@ -17,140 +17,53 @@ class ReminderEditScreen extends StatefulWidget {
 }
 
 class _ReminderEditScreenState extends State<ReminderEditScreen> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _titleController = TextEditingController();
-  DateTime _remindAt = DateTime.now();
-
-  void _pickNotificationDate(BuildContext context) async {
-    final now = DateTime.now();
-    final DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: _remindAt,
-      firstDate: now,
-      lastDate: now.add(const Duration(days: 365)),
-    );
-
-    if (pickedDate == null || pickedDate == _remindAt) {
-      return;
-    }
-
-    if (!context.mounted) {
-      return;
-    }
-
-    final TimeOfDay? pickedTime = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.fromDateTime(_remindAt),
-    );
-
-    if (pickedTime == null) {
-      setState(() {
-        _remindAt = DateTime(
-          pickedDate.year,
-          pickedDate.month,
-          pickedDate.day,
-          _remindAt.hour,
-          _remindAt.minute,
-        );
-      });
-
-      return;
-    }
-
-    setState(() {
-      _remindAt = DateTime(
-        pickedDate.year,
-        pickedDate.month,
-        pickedDate.day,
-        pickedTime.hour,
-        pickedTime.minute,
-      );
-    });
-  }
+  late Future<ReminderEntity> futureReminderEntity;
 
   @override
-  void dispose() {
-    _titleController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+
+    final uid = context.read<UserState>().user?.uid;
+    final id = widget.id;
+
+    if (uid == null || id == null) {
+      futureReminderEntity = Future.value(ReminderEntity(
+        id: '',
+        title: '',
+        remindAt: DateTime.now(),
+      ));
+
+      return;
+    }
+
+    futureReminderEntity = context.read<GetReminderUseCase>()(
+      uid: uid,
+      id: id,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    String formattedRemindAt = DateFormat('yyyy-MM-dd HH:mm').format(_remindAt);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Reminder'),
       ),
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                autofocus: true,
-                controller: _titleController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your title',
-                  labelText: 'Title',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Enter your title to continue';
-                  }
+      body: FutureBuilder(
+        future: futureReminderEntity,
+        builder:
+            (BuildContext context, AsyncSnapshot<ReminderEntity> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              Row(
-                children: [
-                  Expanded(child: Text(formattedRemindAt)),
-                  IconButton(
-                    icon: const Icon(Icons.calendar_today),
-                    onPressed: () => _pickNotificationDate(context),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () async {
-                  if (!_formKey.currentState!.validate()) {
-                    return;
-                  }
+          if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
 
-                  final addReminderUseCase = context.read<AddReminderUseCase>();
-                  final reminderListState = context.read<ReminderListState>();
-                  final uid = context.read<UserState>().user?.uid;
-
-                  if (uid == null) {
-                    return;
-                  }
-
-                  await addReminderUseCase(
-                    uid: uid,
-                    reminderEntity: ReminderEntity(
-                      id: '',
-                      title: _titleController.text,
-                      remindAt: _remindAt,
-                    ),
-                  );
-
-                  _titleController.clear();
-
-                  await reminderListState.getReminders();
-
-                  if (!context.mounted) {
-                    return;
-                  }
-                  context.pop();
-                },
-                child: const Text('Submit'),
-              ),
-            ],
-          ),
-        ),
+          return ReminderEditFormWidget(reminderEntity: snapshot.data!);
+        },
       ),
     );
   }
---
+++ lib/presentation/widget/reminder_edit_form_widget.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:reminder_flutter/application/use_case/reminder/add_reminder_use_case.dart';
+import 'package:reminder_flutter/domain/entity/reminder_entity.dart';
+import 'package:reminder_flutter/presentation/state/reminder_list_state.dart';
+import 'package:reminder_flutter/presentation/state/user_state.dart';
+
+class ReminderEditFormWidget extends StatefulWidget {
+  final ReminderEntity reminderEntity;
+
+  const ReminderEditFormWidget({super.key, required this.reminderEntity});
+
+  @override
+  State<ReminderEditFormWidget> createState() => _ReminderEditFormWidgetState();
+}
+
+class _ReminderEditFormWidgetState extends State<ReminderEditFormWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late final TextEditingController _titleController =
+      TextEditingController(text: widget.reminderEntity.title);
+  late DateTime _remindAt = widget.reminderEntity.remindAt;
+
+  void _pickNotificationDate(BuildContext context) async {
+    final now = DateTime.now();
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _remindAt,
+      firstDate: now,
+      lastDate: now.add(const Duration(days: 365)),
+    );
+
+    if (pickedDate == null || pickedDate == _remindAt) {
+      return;
+    }
+
+    if (!context.mounted) {
+      return;
+    }
+
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(_remindAt),
+    );
+
+    if (pickedTime == null) {
+      setState(() {
+        _remindAt = DateTime(
+          pickedDate.year,
+          pickedDate.month,
+          pickedDate.day,
+          _remindAt.hour,
+          _remindAt.minute,
+        );
+      });
+
+      return;
+    }
+
+    setState(() {
+      _remindAt = DateTime(
+        pickedDate.year,
+        pickedDate.month,
+        pickedDate.day,
+        pickedTime.hour,
+        pickedTime.minute,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedRemindAt = DateFormat('yyyy-MM-dd HH:mm').format(_remindAt);
+
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              controller: _titleController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your title',
+                labelText: 'Title',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Enter your title to continue';
+                }
+
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              children: [
+                Expanded(child: Text(formattedRemindAt)),
+                IconButton(
+                  icon: const Icon(Icons.calendar_today),
+                  onPressed: () => _pickNotificationDate(context),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () async {
+                if (!_formKey.currentState!.validate()) {
+                  return;
+                }
+
+                final addReminderUseCase = context.read<AddReminderUseCase>();
+                final reminderListState = context.read<ReminderListState>();
+                final uid = context.read<UserState>().user?.uid;
+
+                if (uid == null) {
+                  return;
+                }
+
+                await addReminderUseCase(
+                  uid: uid,
+                  reminderEntity: ReminderEntity(
+                    id: '',
+                    title: _titleController.text,
+                    remindAt: _remindAt,
+                  ),
+                );
+
+                _titleController.clear();
+
+                await reminderListState.getReminders();
+
+                if (!context.mounted) {
+                  return;
+                }
+                context.pop();
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
