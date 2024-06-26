7d8b7636dfad9026f0fd290a2049eb3e2e327b87
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-06-26 21:51:42 +0900

    feat: add provider

---
+++ lib/presentation/provider/reminder_repository_provider.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:reminder_flutter/domain/repository/reminder_repository.dart';
+import 'package:reminder_flutter/infrastructure/repository_impl/firestore_reminder_repository_impl.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'reminder_repository_provider.g.dart';
+
+@riverpod
+ReminderRepository reminderRepository(ReminderRepositoryRef ref) {
+  return FirestoreReminderRepositoryImpl(FirebaseFirestore.instance);
+}
