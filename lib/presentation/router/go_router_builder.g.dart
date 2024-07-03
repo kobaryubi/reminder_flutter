d1a85a68369a1b83b24a268021b56b0d76bc8eb6
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-07-03 22:29:32 +0900

    feat: type safe

---
+++ lib/presentation/router/go_router_builder.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'go_router_builder.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $signInRoute,
+    ];
+
+RouteBase get $signInRoute => GoRouteData.$route(
+      path: '/sign-in',
+      factory: $SignInRouteExtension._fromState,
+    );
+
+extension $SignInRouteExtension on SignInRoute {
+  static SignInRoute _fromState(GoRouterState state) => const SignInRoute();
+
+  String get location => GoRouteData.$location(
+        '/sign-in',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
--- lib/presentation/router/router.dart
+++ lib/presentation/router/router.dart
@@ -1,10 +1,7 @@
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:reminder_flutter/presentation/router/go_router_builder.dart';
 import 'package:reminder_flutter/presentation/router/route_paths.dart';
-import 'package:reminder_flutter/presentation/screen/reminder_edit_screen.dart';
-import 'package:reminder_flutter/presentation/screen/reminder_list_screen.dart';
-import 'package:reminder_flutter/presentation/widget/scaffold_with_nav_bar_widget.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -12,109 +9,5 @@ final GlobalKey<NavigatorState> _rootNavigatorKey =
 final router = GoRouter(
   navigatorKey: _rootNavigatorKey,
   initialLocation: RoutePaths.home,
-  routes: <RouteBase>[
-    GoRoute(
-      path: RoutePaths.signIn,
-      builder: (BuildContext context, GoRouterState state) {
-        return SignInScreen(
-          actions: [
-            AuthStateChangeAction<UserCreated>(
-                (BuildContext context, UserCreated state) async {
-              final user = state.credential.user;
-              if (user == null || user.emailVerified) {
-                return;
-              }
-
-              await user.sendEmailVerification();
-              if (!context.mounted) {
-                return;
-              }
-
-              const snackBar = SnackBar(
-                content: Text(
-                    'Please check your email to verify your email address'),
-              );
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            }),
-            AuthStateChangeAction<SignedIn>(
-                (BuildContext context, SignedIn state) async {
-              final user = state.user;
-              if (user == null) {
-                return;
-              }
-
-              if (!user.emailVerified) {
-                await user.sendEmailVerification();
-                if (!context.mounted) {
-                  return;
-                }
-
-                const snackBar = SnackBar(
-                  content: Text(
-                      'Please check your email to verify your email address'),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-                return;
-              }
-
-              context.go(RoutePaths.home);
-            }),
-          ],
-        );
-      },
-    ),
-    StatefulShellRoute.indexedStack(
-      builder: (
-        BuildContext context,
-        GoRouterState state,
-        StatefulNavigationShell navigationShell,
-      ) {
-        return ScaffoldWithNavBar(navigationShell: navigationShell);
-      },
-      branches: <StatefulShellBranch>[
-        StatefulShellBranch(
-          routes: <RouteBase>[
-            GoRoute(
-              path: RoutePaths.home,
-              builder: (BuildContext context, GoRouterState state) {
-                return const ReminderListScreen();
-              },
-            ),
-            GoRoute(
-              path: RoutePaths.reminderAdd,
-              builder: (BuildContext context, GoRouterState state) {
-                return const ReminderEditScreen();
-              },
-            ),
-            GoRoute(
-              path: RoutePaths.reminderEdit,
-              builder: (BuildContext context, GoRouterState state) {
-                return ReminderEditScreen(id: state.pathParameters['id']);
-              },
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          routes: <RouteBase>[
-            GoRoute(
-              path: RoutePaths.profile,
-              builder: (BuildContext context, GoRouterState state) {
-                return Scaffold(
-                  appBar: AppBar(title: const Text('Profile')),
-                  body: ProfileScreen(actions: [
-                    SignedOutAction(
-                      (context) {
-                        context.go(RoutePaths.signIn);
-                      },
-                    )
-                  ]),
-                );
-              },
-            ),
-          ],
-        ),
-      ],
-    ),
-  ],
+  routes: $appRoutes,
 );
