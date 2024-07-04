a0a2ab659ba14575e14ddf5d2dd49afe4d4f5f7a
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-07-04 21:45:21 +0900

    feat: builder

--- lib/presentation/router/go_router_builder.dart
+++ lib/presentation/router/go_router_builder.dart
@@ -2,6 +2,9 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:reminder_flutter/presentation/router/route_paths.dart';
+import 'package:reminder_flutter/presentation/screen/reminder_edit_screen.dart';
+import 'package:reminder_flutter/presentation/screen/reminder_list_screen.dart';
+import 'package:reminder_flutter/presentation/widget/scaffold_with_nav_bar_widget.dart';
 
 part 'go_router_builder.g.dart';
 
@@ -74,9 +77,95 @@ class SignInRoute extends GoRouteData {
             return;
           }
 
-          context.go(RoutePaths.home);
+          const ReminderListRouteData().go(context);
         }),
       ],
     );
+  }
+}
+
+class ReminderBranchData extends StatefulShellBranchData {
+  const ReminderBranchData();
+}
+
+class ReminderListRouteData extends GoRouteData {
+  const ReminderListRouteData();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const ReminderListScreen();
+  }
+}
+
+class ReminderAddRouteData extends GoRouteData {
+  const ReminderAddRouteData();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const ReminderEditScreen();
+  }
+}
+
+class ReminderEditRouteData extends GoRouteData {
+  final String id;
+
+  const ReminderEditRouteData({required this.id});
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return ReminderEditScreen(id: id);
+  }
+}
+
+class AccountBranchData extends StatefulShellBranchData {
+  const AccountBranchData();
+}
+
+class ProfileRouteData extends GoRouteData {
+  const ProfileRouteData();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile')),
+      body: ProfileScreen(
+        actions: [
+          SignedOutAction(
+            (context) {
+              const SignInRoute().go(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+@TypedStatefulShellRoute<ReminderShellRouteData>(
+  branches: <TypedStatefulShellBranch<StatefulShellBranchData>>[
+    TypedStatefulShellBranch<ReminderBranchData>(
+      routes: <TypedRoute<RouteData>>[
+        TypedGoRoute<ReminderListRouteData>(path: RoutePaths.home),
+        TypedGoRoute<ReminderAddRouteData>(path: RoutePaths.reminderAdd),
+        TypedGoRoute<ReminderEditRouteData>(path: RoutePaths.reminderEdit),
+      ],
+    ),
+    TypedStatefulShellBranch<AccountBranchData>(
+      routes: <TypedRoute<RouteData>>[
+        TypedGoRoute<ProfileRouteData>(path: RoutePaths.profile),
+      ],
+    ),
+  ],
+)
+class ReminderShellRouteData extends StatefulShellRouteData {
+  const ReminderShellRouteData();
+
+  @override
+  Widget builder(
+    BuildContext context,
+    GoRouterState state,
+    StatefulNavigationShell navigationShell,
+  ) {
+    return ScaffoldWithNavBar(navigationShell: navigationShell);
   }
 }
