3b0d526c7b3e0c148383dcecb1d5bc2988d4b7a0
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-07-21 07:52:47 +0900

    feat: cancel notification

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # reminder_flutter
 
 A new Flutter project.
+
+## Build
+```sh
+flutter build ipa --obfuscate --split-debug-info=build/app/outputs/symbols 
+```
--- lib/presentation/mixin/reminder_mixin.dart
+++ lib/presentation/mixin/reminder_mixin.dart
@@ -2,16 +2,14 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/intl.dart';
 import 'package:reminder_flutter/domain/entity/reminder_entity.dart';
 import 'package:reminder_flutter/main.dart';
-import 'package:uuid/uuid.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 mixin ReminderMixin {
   void scheduleLocalNotification({
     required ReminderEntity reminderEntity,
   }) async {
-    const uuid = Uuid();
     await flutterLocalNotificationsPlugin.zonedSchedule(
-      uuid.v4().hashCode,
+      reminderEntity.id.hashCode,
       reminderEntity.title,
       DateFormat('yyyy-MM-dd HH:mm').format(reminderEntity.remindAt),
       tz.TZDateTime.from(reminderEntity.remindAt, tz.local),
